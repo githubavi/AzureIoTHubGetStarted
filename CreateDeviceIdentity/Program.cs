5ca16a2ac9b0181433f826b55c98e991aa544d52
Avijit Samanta <avijit.samanta@ipl.edu.in>
2018-08-11 10:17:20 +0000

    iot changes

--- CreateDeviceIdentity/Program.cs
+++ CreateDeviceIdentity/Program.cs
@@ -9,117 +9,110 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client.Samples;
+using TransportType = Microsoft.Azure.Devices.Client.TransportType;
 
 namespace CreateDeviceIdentity
 {
     class Program
     {
         static RegistryManager registryManager;
-        static string connectionString = "HostName=iothubavi.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=leQmYr0PLYzJOyS6WQTlhshSCVK1qH99DAXMsClQvBY=";
+        static string deviceConnectionString = "HostName=iottestavi.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=MN+B2PfQmDezOl+9W+yCPB6P6J1t2gseXHzs6L4JcZ4=";
+        
+        static  string registryConnectionString = "HostName=iottestavi.azure-devices.net;SharedAccessKeyName=registryReadWrite;SharedAccessKey=havzBkGDPzhByLvpeEn3bP+/ATqCaTwK0X2JcbuKV6Y=";
         static string iotHubD2cEndpoint = "messages/events";
-        static EventHubClient eventHubClient;
         static string deviceKey;
         static DeviceClient deviceClient;
-        static string iotHubUri = "iothubavi.azure-devices.net";
+        static string iotHubUri = "iottestavi.azure-devices.net";
 
         static void Main(string[] args)
         {
-            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
-            AddDeviceAsync().Wait();
+            registryManager = RegistryManager.CreateFromConnectionString(registryConnectionString);
+            AddDeviceAsync().GetAwaiter().GetResult();
             Console.ReadLine();
 
             Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
 
             //connectionString = "HostName=iothubavi.azure-devices.net;DeviceId=deviceavi;SharedAccessKey=GrDhkkcm8ZE51l1m0V53NYrHEFquL4Uuf+6TwAU+f6o=";
-            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+            deviceClient = DeviceClient.CreateFromConnectionString(
+                //"HostName=iottestavi.azure-devices.net;DeviceId=deviceavi;SharedAccessKey=Ywt7XBLXN/NRbJc6nKWb65o6mHSWbr7Df+tH3HfTIgs=");
+                $"HostName=iottestavi.azure-devices.net;DeviceId=deviceavi;SharedAccessKey={deviceKey}");
 
-            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
-
-            CancellationTokenSource cts = new CancellationTokenSource();
-
-            Console.CancelKeyPress += (s, e) =>
+            if (deviceClient == null)
             {
-                e.Cancel = true;
-                cts.Cancel();
-                Console.WriteLine("Exiting...");
-                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine("Failed to create DeviceClient!");
+                Console.ReadLine();
                 return;
-            };
-
-            var tasks = new List<Task>();
-            foreach (string partition in d2cPartitions)
-            {
-                tasks.Add(ReceiveMessagesFromHubAsync(partition, cts.Token));
             }
 
-            Console.WriteLine("Simulated device\n");
-            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), Microsoft.Azure.Devices.Client.TransportType.Mqtt);
+            var sample = new MessageSample(deviceClient);
+            sample.RunSampleAsync().GetAwaiter().GetResult();
 
-            SendDeviceToCloudMessagesAsync(cts.Token);
-
-            Task.WaitAll(tasks.ToArray());
-
+            Console.WriteLine("Done.\n");
+           
             Console.ReadLine();
-
         }
         static string deviceId = "deviceavi";
-        private static async Task AddDeviceAsync()
+        private static async Task<string> AddDeviceAsync()
         {
             Device device;
             try
             {
-                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+                device = await registryManager.GetDeviceAsync(deviceId);
+                if(device == null)
+                    device = await registryManager.AddDeviceAsync(new Device(deviceId));
             }
             catch (DeviceAlreadyExistsException)
             {
                 device = await registryManager.GetDeviceAsync(deviceId);
             }
-            deviceKey = device.Authentication.SymmetricKey.PrimaryKey;
+            deviceKey = device.Authentication.SymmetricKey.SecondaryKey;
             Console.WriteLine("Generated device key: {0}", deviceKey);
+            return deviceKey;
         }
 
-        private static async Task ReceiveMessagesFromHubAsync(string partition, CancellationToken ct)
-        {
-            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+        //private static async Task ReceiveMessagesFromHubAsync(string partition, CancellationToken ct)
+        //{
+        //    var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
             
-            while (true)
-            {
-                if (ct.IsCancellationRequested) break;
-                EventData eventData = await eventHubReceiver.ReceiveAsync();
-                if (eventData == null) continue;
+        //    while (true)
+        //    {
+        //        if (ct.IsCancellationRequested) break;
+        //        EventData eventData = await eventHubReceiver.ReceiveAsync();
+        //        if (eventData == null) continue;
 
-                string data = Encoding.UTF8.GetString(eventData.GetBytes());
-                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
-            }
-        }
+        //        string data = Encoding.UTF8.GetString(eventData.GetBytes());
+        //        Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+        //    }
+        //}
 
-        private static async void SendDeviceToCloudMessagesAsync(CancellationToken ct)
-        {
-            double avgWindSpeed = 10; // m/s
-            Random rand = new Random();
-            Random rand2 = new Random();
+        //private static async void SendDeviceToCloudMessagesAsync(CancellationToken ct)
+        //{
+        //    double avgWindSpeed = 10; // m/s
+        //    Random rand = new Random();
+        //    Random rand2 = new Random();
 
-            while (true)
-            {
-                if (ct.IsCancellationRequested) break;
+        //    while (true)
+        //    {
+        //        if (ct.IsCancellationRequested) break;
 
-                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+        //        double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
 
-                var telemetryDataPoint = new
-                {
-                    deviceId = deviceId,
-                    windSpeed = currentWindSpeed,
-                    partition = rand2.Next(0, 5),
-                    runid = Guid.NewGuid()
-                };
-                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
-                var message = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(messageString));
+        //        var telemetryDataPoint = new
+        //        {
+        //            deviceId = deviceId,
+        //            windSpeed = currentWindSpeed,
+        //            partition = rand2.Next(0, 5),
+        //            runid = Guid.NewGuid()
+        //        };
+        //        var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+        //        var message = new Microsoft.Azure.Devices.Client.Message(Encoding.UTF8.GetBytes(messageString));
 
-                await deviceClient.SendEventAsync(message);
-                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+        //        await deviceClient.SendEventAsync(message);
+        //        Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
 
-                await Task.Delay(1000);
-            }
-        }
+        //        await Task.Delay(1000);
+        //    }
+        //}
     }
 }
